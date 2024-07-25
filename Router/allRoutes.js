2e2c8a9a4ab663cda572c33a07ce68799fe207e4
sekou78 <s.cisse78@gmail.com>
2024-07-25 18:24:40 +0200

    Mise en place des routes avec le JS + configuration des routes et Creation des pages

---
+++ Router/allRoutes.js
@@ -0,0 +1,13 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes
+export const allRoutes = [
+  new Route("/", "Accueil", "../pages/home.html"),
+  new Route("/repositories", "Repositories", "../pages/repositories.html"),
+  new Route("/compte", "Compte", "../pages/auth/compte.html"),
+  new Route("/inscription", "Inscription", "../pages/auth/inscription.html"),
+  new Route("/connexion", "Connexion", "../pages/auth/connexion.html"),
+];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Mon Github";
--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("coucou");
+import { Octokit } from "https://esm.sh/octokit";
+
+const test = new Octokit({});
+
+test.rest.repos
+  .get({
+    owner: "sekou78",
+    repo: "restaurant",
+  })
+  .then(({ data }) => {
+    // console.log("Réçu", data);
+    // console.log(data.owner);
+  });
+
+// console.log(test);
+// console.log(test.rest.users);
